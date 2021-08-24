bfb0d415296c7a440d33d725df17b60769001015
brunoav91@gmail.com <brunoav91@gmail.com>
2021-08-23 22:13:29 -0300

    iniciando projeto

---
+++ src/test/java/com/boaentrega/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boaentrega.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
